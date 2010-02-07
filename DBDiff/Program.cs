86d731de5de9520c6bed0477a3f1b39520c6ec07
dean.thomas <dean.thomas@4f8e7f76-137e-11df-8288-654b40affd92>
2010-02-07 01:28:22 +0000

    Refactored the 'PrincipalForm' form, it was called Form1, which isn't very helpful.

--- DBDiff/Program.cs
+++ DBDiff/Program.cs
@@ -15,7 +15,7 @@ namespace DBDiff
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PrincipalForm());
         }
     }
 }
