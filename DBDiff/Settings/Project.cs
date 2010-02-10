aa1a89325fc192cdaebd723726c4b9e5615b321b
dean.thomas <dean.thomas@4f8e7f76-137e-11df-8288-654b40affd92>
2010-02-10 00:11:42 +0000

    Removed references to SQlite, we now use the SqlServerCE as it's easier to use and it's part of .NET 3.5. One less external lib. Updated the project logic to use the new 'Database.sdf' file, which is a SqlServerCe database.

--- DBDiff/Settings/Project.cs
+++ DBDiff/Settings/Project.cs
@@ -2,86 +2,113 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Data.SQLite;
+using System.Data;
 using DBDiff.Schema.SQLServer.Generates.Options;
+using System.Data.SqlServerCe;
 
 namespace DBDiff.Settings
 {
     public class Project
     {
-        public enum ProjectType
+        #region SQL Statements
+        private static string ProjectUpdateSql = @"UPDATE Project SET Name = @Name, ConnectionStringSource = @ConnectionStringSource, ConnectionStringDestination = @ConnectionStringDestination, Type = @Type WHERE ProjectId = @ProjectId";
+        #endregion
+
+        public enum ProjectType : short
         {
             SQLServer = 1
         }
 
-        private const string connectionSQLLite = "Data Source=Settings.conf;Pooling=true;FailIfMissing=false";
-
+        private const string connectionString = "Data Source=Database.sdf";
         public int Id { get; set; }
         public string ConnectionStringSource { get; set; }
         public string ConnectionStringDestination { get; set; }
         public SqlOption Options { get; set; }
         public ProjectType Type { get; set; }
         public string Name { get; set; }
- 
+
+        #region Private Static Methods
+        /// <summary>
+        /// Add a Project to the list in the databse
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         private static int Add(Project item)
         {
             int maxId = 0;
-            using (SQLiteConnection connection = new SQLiteConnection(connectionSQLLite))
+            using (SqlCeConnection connection = new SqlCeConnection(connectionString))
             {
                 connection.Open();
-                using (SQLiteTransaction transaction = connection.BeginTransaction())
+                using (SqlCeTransaction transaction = connection.BeginTransaction())
                 {
-                    using (
-                        SQLiteCommand command = new SQLiteCommand("INSERT INTO Project (Name, ConnectionStringSource, ConnectionStringDestination, Options, Type, Internal) VALUES ('" + item.Name.Replace("'","''") + "','" + item.ConnectionStringSource + "','" + item.ConnectionStringDestination + "','" + item.Options + "'," + ((int) item.Type).ToString() + ",0)", connection, transaction))
+                    using (SqlCeCommand command = new SqlCeCommand("INSERT INTO Project (Name, ConnectionStringSource, ConnectionStringDestination, Options, Type, Internal) VALUES ('" + item.Name.Replace("'", "''") + "','" + item.ConnectionStringSource + "','" + item.ConnectionStringDestination + "','" + item.Options + "'," + ((int)item.Type).ToString() + ",0)", connection, transaction))
                     {
                         command.ExecuteNonQuery();
-                    }
-                    using (SQLiteCommand command = new SQLiteCommand("SELECT MAX(ProjectId) AS NewId FROM Project WHERE Internal = 0",connection, transaction))
-                    {
-                        using (SQLiteDataReader reader = command.ExecuteReader())
-                        {
+
+                        command.CommandText = "SELECT MAX(ProjectId) AS NewId FROM Project WHERE Internal = 0";
+                    
+                        using (SqlCeDataReader reader = command.ExecuteReader())
                             if (reader.Read())
-                            {
                                 maxId = int.Parse(reader["NewId"].ToString());
-                            }
-                        }
                     }
+
                     transaction.Commit();
                 }
             }
             return maxId;
         }
 
+        /// <summary>
+        /// Update an existing item in the Project List
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         private static int Update(Project item)
         {
-            using (SQLiteConnection connection = new SQLiteConnection(connectionSQLLite))
+            using (SqlCeConnection connection = new SqlCeConnection(connectionString))
             {
                 connection.Open();
-                using (SQLiteCommand command = new SQLiteCommand("UPDATE Project SET Name = '" + item.Name.Replace("'","''") + "', ConnectionStringSource = '" + item.ConnectionStringSource + "', ConnectionStringDestination = '" + item.ConnectionStringDestination + "', Type = " + ((int)item.Type).ToString() + " WHERE ProjectId = " + item.Id.ToString(),connection))
+                using (SqlCeCommand command = new SqlCeCommand(ProjectUpdateSql, connection))
                 {
-                    command.ExecuteNonQuery(); 
+                    command.Parameters.Add("@Name", SqlDbType.NVarChar).Value = item.Name;
+                    command.Parameters.Add("@ConnectionStringSource", SqlDbType.NVarChar).Value = item.ConnectionStringSource;
+                    command.Parameters.Add("@ConnectionStringDestination", SqlDbType.NVarChar).Value = item.ConnectionStringDestination;
+                    command.Parameters.Add("@Type", SqlDbType.Int).Value = item.Type;
+                    command.Parameters.Add("@ProjectId", SqlDbType.Int).Value = item.Id;
+
+                    command.ExecuteNonQuery();
                 }
             }
             return item.Id;
         }
+        #endregion
 
+        /// <summary>
+        /// Delete an item from the project list
+        /// </summary>
+        /// <param name="Id"></param>
         public static void Delete(int Id)
         {
-            using (SQLiteConnection connection = new SQLiteConnection(connectionSQLLite))
+            using (SqlCeConnection connection = new SqlCeConnection(connectionString))
             {
                 connection.Open();
-                using (SQLiteCommand command = new SQLiteCommand("DELETE FROM Project WHERE ProjectId = " + Id.ToString(), connection))
+                using (SqlCeCommand command = new SqlCeCommand("DELETE FROM Project WHERE ProjectId = " + Id.ToString(), connection))
                 {
                     command.ExecuteNonQuery();
                 }
             }
         }
 
+        /// <summary>
+        /// Save an item, decide whether to insert or update.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public static int Save(Project item)
         {
             if (item.Id == 0)
                 return Add(item);
-            
+
             return Update(item);
         }
 
@@ -89,21 +116,21 @@ namespace DBDiff.Settings
         {
             if (GetLastConfiguration() != null)
             {
-                using (SQLiteConnection connection = new SQLiteConnection(connectionSQLLite))
+                using (SqlCeConnection connection = new SqlCeConnection(connectionString))
                 {
                     connection.Open();
-                    using (SQLiteCommand command = new SQLiteCommand("UPDATE Project SET ConnectionStringSource = '" + ConnectionStringSource + "', ConnectionStringDestination = '" + ConnectionStringDestination + "' WHERE Internal = 1", connection))
+                    using (SqlCeCommand command = new SqlCeCommand("UPDATE Project SET ConnectionStringSource = '" + ConnectionStringSource + "', ConnectionStringDestination = '" + ConnectionStringDestination + "' WHERE Internal = 1", connection))
                     {
                         command.ExecuteNonQuery();
                     }
-                }                
+                }
             }
             else
             {
-                using (SQLiteConnection connection = new SQLiteConnection(connectionSQLLite))
+                using (SqlCeConnection connection = new SqlCeConnection(connectionString))
                 {
                     connection.Open();
-                    using (SQLiteCommand command = new SQLiteCommand("INSERT INTO Project (Name, ConnectionStringSource, ConnectionStringDestination, Options, Type, Internal) VALUES ('LastConfiguration','" + ConnectionStringSource + "','" + ConnectionStringDestination + "','',1,1)", connection))
+                    using (SqlCeCommand command = new SqlCeCommand("INSERT INTO Project (Name, ConnectionStringSource, ConnectionStringDestination, Options, Type, Internal) VALUES ('LastConfiguration','" + ConnectionStringSource + "','" + ConnectionStringDestination + "','',1,1)", connection))
                     {
                         command.ExecuteNonQuery();
                     }
@@ -114,54 +141,51 @@ namespace DBDiff.Settings
         public static Project GetLastConfiguration()
         {
             Project item = null;
-            using (SQLiteConnection connection = new SQLiteConnection(connectionSQLLite))
+            using (SqlCeConnection connection = new SqlCeConnection(connectionString))
             {
                 connection.Open();
-                using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM Project WHERE Internal = 1 ORDER BY Name", connection))
+                using (SqlCeCommand command = new SqlCeCommand("SELECT * FROM Project WHERE Internal = 1 ORDER BY Name", connection))
                 {
-                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    using (SqlCeDataReader reader = command.ExecuteReader())
                     {
                         if (reader.Read())
                         {
-                            item = new Project
+                            item = new Project()
                             {
                                 Id = int.Parse(reader["ProjectId"].ToString()),
                                 ConnectionStringSource = reader["ConnectionStringSource"].ToString(),
                                 ConnectionStringDestination = reader["ConnectionStringDestination"].ToString(),
-                                Type = (ProjectType)(long)reader["Type"],
-                                //Options = (SqlOption) reader["Options"],
+                                Type = (ProjectType)Convert.ToInt16(reader["Type"]),
                                 Name = reader["Name"].ToString()
-                            };                            
+                            };
                         }
                     }
                 }
             }
             return item;
-            
+
         }
 
         public static List<Project> GetAll()
         {
             List<Project> items = new List<Project>();
-            using (SQLiteConnection connection = new SQLiteConnection(connectionSQLLite))
+            using (SqlCeConnection connection = new SqlCeConnection(connectionString))
             {
                 connection.Open();
-                using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM Project WHERE Internal = 0 ORDER BY Name",connection))
+                using (SqlCeCommand command = new SqlCeCommand("SELECT * FROM Project WHERE Internal = 0 ORDER BY Name", connection))
                 {
-                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    using (SqlCeDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            Project item = new Project
-                                               {
-                                                   Id = int.Parse(reader["ProjectId"].ToString()),
-                                                   ConnectionStringSource = reader["ConnectionStringSource"].ToString(),
-                                                   ConnectionStringDestination =
-                                                       reader["ConnectionStringDestination"].ToString(),
-                                                   Type = (ProjectType) (long) reader["Type"],
-                                                   //Options = (SqlOption) reader["Options"],
-                                                   Name = reader["Name"].ToString()
-                                               };
+                            Project item = new Project()
+                            {
+                                Id = int.Parse(reader["ProjectId"].ToString()),
+                                ConnectionStringSource = reader["ConnectionStringSource"].ToString(),
+                                ConnectionStringDestination = reader["ConnectionStringDestination"].ToString(),
+                                Type = (ProjectType)Convert.ToInt16(reader["Type"]),
+                                Name = reader["Name"].ToString()
+                            };
                             items.Add(item);
                         }
                     }
