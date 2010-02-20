df7c04fbdb4988267a60329c2ba6d68a991ced0f
dean.thomas <dean.thomas@4f8e7f76-137e-11df-8288-654b40affd92>
2010-02-20 15:20:09 +0000

    Reformatted code file, added logic which now writes out the number of Alter, Create, Drop objects in the database, so you get a brief overview.

--- DBDiff/Front/SchemaTreeView.cs
+++ DBDiff/Front/SchemaTreeView.cs
@@ -11,6 +11,7 @@ using System.Reflection;
 using DBDiff.Schema;
 using DBDiff.Schema.Model;
 using DBDiff.Schema.Attributes;
+using DBDiff.Schema.SQLServer.Generates.Model;
 
 namespace DBDiff.Front
 {
@@ -18,7 +19,7 @@ namespace DBDiff.Front
     {
         private ISchemaBase databaseSource;
         private ISchemaBase databaseDestination;
-        
+
         public delegate void SchemaHandler(string ObjectFullName);
         public event SchemaHandler OnSelectItem;
 
@@ -36,8 +37,8 @@ namespace DBDiff.Front
         public ISchemaBase DatabaseSource
         {
             get { return databaseSource; }
-            set 
-            { 
+            set
+            {
                 databaseSource = value;
                 if (value != null)
                 {
@@ -47,7 +48,7 @@ namespace DBDiff.Front
         }
 
         private void ReadPropertys(Type item, TreeNodeCollection nodes, ISchemaBase schema)
-        {                        
+        {
             PropertyInfo[] pi = item.GetProperties();
             nodes.Clear();
             foreach (PropertyInfo p in pi)
@@ -63,33 +64,51 @@ namespace DBDiff.Front
             }
         }
 
+        public static IList<T> ConvertToListOf<T>(IList iList)
+        {
+            IList<T> result = new List<T>();
+            foreach (T value in iList)
+                result.Add(value);
+            return result;
+        }        
         private void ReadPropertyDetail(TreeNode node, PropertyInfo p, ISchemaBase schema, ShowItemAttribute attr)
         {
-            IList items = (IList)p.GetValue(schema, null);
-            node.Text = node.Text + " (" + items.Count + ")";
+            List<SQLServerSchemaBase> items = SchemaTreeView.ConvertToListOf<SQLServerSchemaBase>(p.GetValue(schema, null) as IList) as List<SQLServerSchemaBase>;
+           
+            //Calculate how many are alter, create, drop, so we can display an easy number. Use Linq, simple and easy.
+            int alterCount, createCount, dropCount;
+
+            alterCount = (from a in items
+                          where a.Status == Enums.ObjectStatusType.AlterStatus || a.Status == Enums.ObjectStatusType.AlterBodyStatus
+                          select a).Count();
+
+            createCount = (from a in items
+                           where a.Status == Enums.ObjectStatusType.CreateStatus
+                           select a).Count();
+
+            dropCount = (from a in items
+                         where a.Status == Enums.ObjectStatusType.DropStatus
+                         select a).Count();
+
+            node.Text = string.Format("{0} - {1} Items (A{2}, C{3}, D{4})", node.Text, items.Count, alterCount, createCount, dropCount);
+
             node.Nodes.Clear();
             foreach (ISchemaBase item in items)
             {
                 if (CanNodeAdd(item))
                 {
-                    TreeNode subnode = node.Nodes.Add((attr.IsFullName?item.FullName:item.Name));
+                    TreeNode subnode = node.Nodes.Add((attr.IsFullName ? item.FullName : item.Name));
                     if (item.Status == Enums.ObjectStatusType.DropStatus)
-                    {
                         subnode.ForeColor = Color.Red;
-                    }
-                    if (item.Status == Enums.ObjectStatusType.CreateStatus)
-                    {
+                    else if (item.Status == Enums.ObjectStatusType.CreateStatus)
                         subnode.ForeColor = Color.Green;
-                    }
-                    if ((item.HasState(Enums.ObjectStatusType.AlterStatus)) || (item.HasState(Enums.ObjectStatusType.RebuildStatus)) || (item.HasState(Enums.ObjectStatusType.DisabledStatus)))
-                    {
+                    else if ((item.HasState(Enums.ObjectStatusType.AlterStatus)) || (item.HasState(Enums.ObjectStatusType.RebuildStatus)) || (item.HasState(Enums.ObjectStatusType.DisabledStatus)))
                         subnode.ForeColor = Color.Blue;
-                    }
                     subnode.Tag = item;
                     subnode.ImageKey = attr.Image;
                     subnode.SelectedImageKey = attr.Image;
                 }
-            }            
+            }
         }
 
         private void RebuildSchemaTree()
@@ -106,17 +125,17 @@ namespace DBDiff.Front
 
         private Boolean CanNodeAdd(ISchemaBase item)
         {
-            if ((item.Status == Enums.ObjectStatusType.DropStatus) && (FilterMissingObjects)) 
+            if ((item.Status == Enums.ObjectStatusType.DropStatus) && (FilterMissingObjects))
                 return true;
-            if ((item.Status == Enums.ObjectStatusType.CreateStatus) && (FilterNewObjects)) 
+            if ((item.Status == Enums.ObjectStatusType.CreateStatus) && (FilterNewObjects))
                 return true;
-            if ((item.Status == Enums.ObjectStatusType.AlterStatus) && (FilterDiferentObjects)) 
+            if ((item.Status == Enums.ObjectStatusType.AlterStatus) && (FilterDiferentObjects))
                 return true;
-            if ((item.Status == Enums.ObjectStatusType.RebuildStatus) && (FilterDiferentObjects)) 
+            if ((item.Status == Enums.ObjectStatusType.RebuildStatus) && (FilterDiferentObjects))
                 return true;
-            if ((item.Status == Enums.ObjectStatusType.DisabledStatus) && (FilterDiferentObjects)) 
+            if ((item.Status == Enums.ObjectStatusType.DisabledStatus) && (FilterDiferentObjects))
                 return true;
-            if ((item.Status == Enums.ObjectStatusType.UpdateStatus) && (FilterDiferentObjects)) 
+            if ((item.Status == Enums.ObjectStatusType.UpdateStatus) && (FilterDiferentObjects))
                 return true;
 
             return false;
